6771c4a1e3c75ec0fd377a39c1ada50c5e69bf9a
Ay-ob <ayoubhadjyoucef1@gmail.com>
2024-09-24 01:55:17 +0100

    ✨ feat: add react router dom and configur it

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
-  return <p>hello world</p>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <div>
+              <h1>Home</h1>
+            </div>
+          }
+        />
+        <Route
+          path='/about'
+          element={
+            <div>
+              <h1>About</h1>
+            </div>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
